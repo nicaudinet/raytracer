9d69a77865a82694e41ed502ab82aae0a7d9886b
Nicolas Audinet <nic.audinet27@gmail.com>
2020-11-07 00:38:53 +0200

    Tests for the tuple module

--- test/Test.hs
+++ test/Test.hs
@@ -1,4 +1,14 @@
 module Main where
 
+import Test.Tasty
+import qualified Test.RayTracer.Tuple as Tuple
+import qualified Test.RayTracer.Color as Color
+
 main :: IO ()
-main = print "some test or something"
+main = defaultMain tests
+
+tests :: TestTree
+tests = testGroup "Tests"
+  [ Tuple.tests
+  , Color.tests
+  ]
