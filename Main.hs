3e8bcd6aa9dbb68f06880f67af58c93e3dfc897d
Nicolas Audinet <nic.audinet27@gmail.com>
2020-11-15 19:45:01 +0200

    Three sphere scene

--- Main.hs
+++ Main.hs
@@ -7,6 +7,8 @@ import RayTracer.Matrix hiding (height, width)
 import RayTracer.Ray as R
 import RayTracer.Sphere
 import RayTracer.Light
+import RayTracer.Camera hiding (pixelSize)
+import RayTracer.World
 
 data Projectile = Projectile
   { pos :: Tuple
@@ -119,11 +121,108 @@ sphere = savePPM "images/sphere.ppm" (canvasToPPM image)
           Just intersection ->
             let
               p = R.position ray (time intersection)
-              normal = normalAt shape p
+              normal = normalAt (Object shape) p
               eye = neg (direction ray)
               m = material . unObject . object $ intersection
               c = lighting m light p eye normal
             in writePixel x y c canvas
 
+threeSphereScene :: IO ()
+threeSphereScene = savePPM "images/threeSphereScene.ppm" (canvasToPPM image)
+  where
+    wallMaterial :: Material
+    wallMaterial = defaultMaterial { color = Color 1 0.9 0.9 , specular = 0 }
+
+    floorSphere :: Sphere
+    floorSphere = Sphere (scaling 10 0.01 10) wallMaterial
+
+    leftWallTransform :: Matrix
+    leftWallTransform
+      =   translation 0 0 5
+      |*| rotationY (-pi / 4)
+      |*| rotationX (pi / 2)
+      |*| scaling 10 0.01 10
+
+    leftWall :: Sphere
+    leftWall = Sphere leftWallTransform wallMaterial
+
+    rightWallTransform :: Matrix
+    rightWallTransform
+      =   translation 0 0 5
+      |*| rotationY (pi / 4)
+      |*| rotationX (pi / 2)
+      |*| scaling 10 0.01 10
+
+    rightWall :: Sphere
+    rightWall = Sphere rightWallTransform wallMaterial
+
+    middleSphereMaterial :: Material
+    middleSphereMaterial = defaultMaterial
+      { color = Color 0.1 1 0.5
+      , diffuse = 0.7
+      , specular = 0.3
+      }
+    
+    middleSphere :: Sphere
+    middleSphere = Sphere (translation (-0.5) 1 0.5) middleSphereMaterial
+
+    rightSphereTransform :: Matrix
+    rightSphereTransform = translation 1.5 0.5 (-0.5) |*| scaling 0.5 0.5 0.5
+
+    rightSphereMaterial :: Material
+    rightSphereMaterial = defaultMaterial
+      { color = Color 0.5 1 0.1
+      , diffuse = 0.7
+      , specular = 0.3
+      }
+
+    rightSphere :: Sphere
+    rightSphere = Sphere rightSphereTransform rightSphereMaterial
+
+    leftSphereTransform :: Matrix
+    leftSphereTransform =
+      translation (-1.5) 0.33 (-0.75) |*| scaling 0.33 0.33 0.33
+
+    leftSphereMaterial :: Material
+    leftSphereMaterial = defaultMaterial
+      { color = Color 1 0.8 0.1
+      , diffuse = 0.7
+      , specular = 0.3
+      }
+
+    leftSphere :: Sphere
+    leftSphere = Sphere leftSphereTransform leftSphereMaterial
+
+    light :: Light
+    light = pointLight (point (-10) 10 (-10)) (Color 1 1 1)
+
+    cameraViewTransform :: Matrix
+    cameraViewTransform =
+      viewTransform (point 0 1.5 (-5)) (point 0 1 0) (vector 0 1 0)
+
+    camera :: Camera
+    camera = Camera
+      { cameraHsize = 100
+      , cameraVsize = 50
+      , cameraFieldOfView = pi / 2
+      , cameraTransform = cameraViewTransform
+      }
+
+    spheres :: [Sphere]
+    spheres =
+      [ floorSphere
+      , leftWall
+      , rightWall
+      , middleSphere
+      , rightSphere
+      , leftSphere
+      ]
+
+    world :: World
+    world = World (map Object spheres) (Just light)
+
+    image :: Canvas
+    image = render camera world
+
 main :: IO ()
-main = sphere
+main = threeSphereScene
